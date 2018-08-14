067b8f1a6a645e9c177930423c778e898200b6ef
Administrator <Administrator@SKY-20180506JSL>
2018-08-14 23:19:48 +0800

    整理

--- abstractFactory/src/main/java/com/manqingxing/abstractFactory/Color.java
+++ abstractFactory/src/main/java/com/manqingxing/abstractFactory/Color.java
@@ -1,11 +1,3 @@
-/**
- * File Name:Color.java
- * Package Name:com.manqingxing.abstractFactory
- * Create Time:2018年8月14日下午11:05:52
- * <p>Copyright Reserved ®2018 Tensorlib.com  </p>
- *
-*/
-
 package com.manqingxing.abstractFactory;
 
 public interface Color {
